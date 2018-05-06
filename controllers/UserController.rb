3ff4ab6c9469e63f0d176c2729f63dfd48437560
Irwin <irgranados16@gmail.com>
2018-05-06 15:45:23 -0500

    Login works

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -13,9 +13,25 @@ class UserController < ApplicationController
 	end
 
 	post '/login' do
-		params.to_json
+
+		# params.to_json
+		
+		@user = User.find_by(username: params[:username])
+
+		if @user && @user.password == params[:password]
+			session[:username] = @user.username
+			session[:logged_in] = true
+			session[:message] = "Logged in as #{@user.username}"
+			redirect '/items'
+
+		else 
+			session[:message] = "Invalid username or password."
+			redirect '/user/login'
+		end
+
 	end
 
+#why the post route only work with the user/register?
 	post '/user/register' do
 		params.to_json
 	end
